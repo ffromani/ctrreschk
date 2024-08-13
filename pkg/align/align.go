2b1d3fb0eeb27cabf6319bb4a3f66ed10757aedc
Francesco Romani <fromani@redhat.com>
2024-08-13 12:31:19 +0200

    pkg: aling: bootstrap unit tests
    bootstrap unit tests
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/align/align.go
+++ pkg/align/align.go
@@ -30,77 +30,6 @@ import (
 	"github.com/ffromani/ctrreschk/pkg/resources"
 )
 
-// Reverse ID MAP (PhysicalID|LLCID|NUMAID) -> LogicalIDs
-type ridMap map[int][]int
-
-func (rm ridMap) CPUSet(id int) cpuset.CPUSet {
-	return cpuset.New(rm[id]...)
-}
-
-func (rm ridMap) String() string {
-	var sb strings.Builder
-	for key, values := range rm {
-		fmt.Fprintf(&sb, " %02d->[%s]", key, cpuset.New(values...).String())
-	}
-	return sb.String()[1:]
-}
-
-// Resource MAPping
-type rMap struct {
-	cpuLog2Phy map[int]int
-	cpuPhy2Log ridMap
-	llc        ridMap
-	numa       ridMap
-}
-
-func (rm rMap) String() string {
-	return fmt.Sprintf("<phys={%s} llc={%s} numa{%s}>", rm.cpuPhy2Log.String(), rm.llc.String(), rm.numa.String())
-}
-
-func newRMap() rMap {
-	return rMap{
-		cpuLog2Phy: make(map[int]int),
-		cpuPhy2Log: make(ridMap),
-		llc:        make(ridMap),
-		numa:       make(ridMap),
-	}
-}
-
-func makeRMap(topo *topology.Info) rMap {
-	res := newRMap()
-	llcID := 0
-	for _, node := range topo.Nodes {
-		for _, core := range node.Cores {
-			phys := res.cpuPhy2Log[core.ID]
-			phys = append(phys, core.LogicalProcessors...)
-			res.cpuPhy2Log[core.ID] = phys
-			for _, lid := range core.LogicalProcessors {
-				res.cpuLog2Phy[lid] = core.ID
-			}
-
-			numa := res.numa[node.ID]
-			numa = append(numa, core.LogicalProcessors...)
-			res.numa[node.ID] = numa
-		}
-		// TODO: yes, we assume LLC=L3.
-		for _, cache := range node.Caches {
-			if cache.Level < 3 {
-				continue
-			}
-
-			llc := res.llc[llcID]
-			for _, id := range cache.LogicalProcessors {
-				llc = append(llc, int(id))
-			}
-			res.llc[llcID] = llc
-
-			llcID += 1
-		}
-	}
-
-	return res
-}
-
 func Check(env *environ.Environ, container resources.Resources, machine machine.Machine) (apiv0.Allocation, error) {
 	rmap := makeRMap(machine.Topology)
 	env.Log.V(2).Info("reverse mapping", "rmap", rmap)
@@ -182,4 +111,75 @@ func checkNUMA(env *environ.Environ, resp *apiv0.Allocation, cores cpuset.CPUSet
 		}
 		resp.Unaligned.NUMA.CPUs = cores.List()
 	}
+}
+
+// Reverse ID MAP (PhysicalID|LLCID|NUMAID) -> LogicalIDs
+type ridMap map[int][]int
+
+func (rm ridMap) CPUSet(id int) cpuset.CPUSet {
+	return cpuset.New(rm[id]...)
+}
+
+func (rm ridMap) String() string {
+	var sb strings.Builder
+	for key, values := range rm {
+		fmt.Fprintf(&sb, " %02d->[%s]", key, cpuset.New(values...).String())
+	}
+	return sb.String()[1:]
+}
+
+// Resource MAPping
+type rMap struct {
+	cpuLog2Phy map[int]int
+	cpuPhy2Log ridMap
+	llc        ridMap
+	numa       ridMap
+}
+
+func (rm rMap) String() string {
+	return fmt.Sprintf("<phys={%s} llc={%s} numa{%s}>", rm.cpuPhy2Log.String(), rm.llc.String(), rm.numa.String())
+}
+
+func newRMap() rMap {
+	return rMap{
+		cpuLog2Phy: make(map[int]int),
+		cpuPhy2Log: make(ridMap),
+		llc:        make(ridMap),
+		numa:       make(ridMap),
+	}
+}
+
+func makeRMap(topo *topology.Info) rMap {
+	res := newRMap()
+	llcID := 0
+	for _, node := range topo.Nodes {
+		for _, core := range node.Cores {
+			phys := res.cpuPhy2Log[core.ID]
+			phys = append(phys, core.LogicalProcessors...)
+			res.cpuPhy2Log[core.ID] = phys
+			for _, lid := range core.LogicalProcessors {
+				res.cpuLog2Phy[lid] = core.ID
+			}
+
+			numa := res.numa[node.ID]
+			numa = append(numa, core.LogicalProcessors...)
+			res.numa[node.ID] = numa
+		}
+		// TODO: yes, we assume LLC=L3.
+		for _, cache := range node.Caches {
+			if cache.Level < 3 {
+				continue
+			}
+
+			llc := res.llc[llcID]
+			for _, id := range cache.LogicalProcessors {
+				llc = append(llc, int(id))
+			}
+			res.llc[llcID] = llc
+
+			llcID += 1
+		}
+	}
+
+	return res
 }
